0d1b1669d857841fdd5487f125bce0a276d0c4cf
Katerina <katerinalupacheva@gmail.com>
2020-04-08 15:44:04 +0300

    Styled added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import ProgressBar from './progress-bar.component';
 
 function App() {
   return (
     <div className="App">
-      
+        <ProgressBar bgcolor={'blue'} completed={40}/>
     </div>
   );
 }
--- src/progress-bar.component.js
+++ src/progress-bar.component.js
@@ -2,10 +2,33 @@ import React from "react";
 
 const ProgressBar = (props) => {
   const { bgcolor, completed } = props;
+
+  const containerStyles = {
+    height: 20,
+    width: '100%',
+    backgroundColor: "#e0e0de",
+    borderRadius: 50,
+    margin: 50
+  }
+
+  const fillerStyles = {
+    height: '100%',
+    width: `${completed}%`,
+    backgroundColor: bgcolor,
+    borderRadius: 'inherit',
+    textAlign: 'right'
+  }
+
+  const labelStyles = {
+    padding: 5,
+    color: 'white',
+    fontWeight: 'bold'
+  }
+
   return (
-    <div bgcolor={bgcolor} completed={`${completed}%`}>
-      <div className="filler">
-        <span>{completed}</span>
+    <div style={containerStyles}>
+      <div style={fillerStyles}>
+        <span style={labelStyles}>{`${completed}%`}</span>
       </div>
     </div>
   );
